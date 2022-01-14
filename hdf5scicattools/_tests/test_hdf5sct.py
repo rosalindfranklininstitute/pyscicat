5469ff41cb137c8b4564a1f26b0657212b111c7a
Brian Richard Pauw <brian.pauw@bam.de>
2022-01-14 18:41:26 +0100

    removed unused import

--- hdf5scicattools/_tests/test_hdf5sct.py
+++ hdf5scicattools/_tests/test_hdf5sct.py
@@ -1,7 +1,6 @@
 # these need to be loaded at the beginning to avoid errors related to relative imports (ImportWarning in h5py)
 # might be related to the change of import style for Python 3.7+. Tested on Python 3.9 at 20220114
 from pathlib import Path
-import os
 
 # these packages are failing to import in McHat if they are not loaded here:
 from ..extractscientificmetadata import extractScientificMetadata
