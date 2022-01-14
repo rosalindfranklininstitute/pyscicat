479faa53d793f70399ac9d29adb53e4bb96c843a
Brian Richard Pauw <brian.pauw@bam.de>
2022-01-14 19:23:45 +0100

    test name change fix

--- pyscicat/hdf5/_tests/test_hdf5sct.py
+++ pyscicat/hdf5/_tests/test_hdf5sct.py
@@ -40,9 +40,7 @@ def test_readMixedDict():
 def test_readMetadata_withroot():
     p = sorted(Path(".").glob("**/cylinderHex_r5_s12_T50_large_ranW_0p5.nxs"))[0]
     assert p.exists(), f"HDF5/NeXus test file: {p.as_posix()} cannot be found"
-    resultDict = extractScientificMetadata(
-        p, excludeRootEntry=True, skipKeyList=["sasdata1"]
-    )
+    resultDict = scientific_metadata(p, excludeRootEntry=True, skipKeyList=["sasdata1"])
     assert resultDict is not None, "extractScientificMetadata has not returned anything"
 
     # make sure the root entry has been skipped
