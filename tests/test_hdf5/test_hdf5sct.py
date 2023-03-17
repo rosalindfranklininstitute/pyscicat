3f3f542159c0f503781f017f0070e36b07f3f7ef
vpf26432 <laura.shemilt@rfi.ac.uk>
2023-03-17 13:57:17 +0000

    Restructured the code so that we can install the optional requiments using `pip install -e ".[option]"`. To add optional requirements add to the [optional.extras_require] in the setup config. All functions and imports work the same way. Restructured the testing suite so it follows a similar pattern. All tests pass.

--- tests/test_hdf5/test_hdf5sct.py
+++ tests/test_hdf5/test_hdf5sct.py
@@ -9,19 +9,19 @@ from pyscicat.hdf5.h5tools import h5Get, h5GetDict
 
 def test_readValue():
     # more intelligent path finding:
-    p = sorted(Path(".").glob("**/cylinderHex_r5_s12_T50_large_ranW_0p5.nxs"))[0]
+    p = sorted(Path("").glob("**/cylinderHex_r5_s12_T50_large_ranW_0p5.nxs"))[0]
     v = h5Get(p, "/sasentry1/sasdata1/I")
     assert v != "none", "Did not extract value"
 
 
 def test_readAttribute():
-    p = sorted(Path(".").glob("**/cylinderHex_r5_s12_T50_large_ranW_0p5.nxs"))[0]
+    p = sorted(Path("").glob("**/cylinderHex_r5_s12_T50_large_ranW_0p5.nxs"))[0]
     v = h5Get(p, "/sasentry1/sasdata1@timestamp")
     assert v != "none", "Did not extract attribute"
 
 
 def test_readMixedDict():
-    p = sorted(Path(".").glob("**/cylinderHex_r5_s12_T50_large_ranW_0p5.nxs"))[0]
+    p = sorted(Path("").glob("**/cylinderHex_r5_s12_T50_large_ranW_0p5.nxs"))[0]
     v = h5GetDict(
         p,
         {
@@ -38,7 +38,7 @@ def test_readMixedDict():
 
 
 def test_readMetadata_withroot():
-    p = sorted(Path(".").glob("**/cylinderHex_r5_s12_T50_large_ranW_0p5.nxs"))[0]
+    p = sorted(Path("").glob("**/cylinderHex_r5_s12_T50_large_ranW_0p5.nxs"))[0]
     assert p.exists(), f"HDF5/NeXus test file: {p.as_posix()} cannot be found"
     resultDict = scientific_metadata(p, excludeRootEntry=True, skipKeyList=["sasdata1"])
     assert resultDict is not None, "scientific_metadata has not returned anything"
