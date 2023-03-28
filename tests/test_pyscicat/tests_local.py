45ee8fad8f7c9eb1026676377548a6e9720d0145
vpf26432 <laura.shemilt@rfi.ac.uk>
2023-03-28 10:31:15 +0000

    Cannot see any differences is the dictionary generated natively and the one generated from pydantic base Model. Try to replicated the error seen with pyscicat by uploading a minimal natively made dictionary. We observe the same error.

--- tests/test_pyscicat/tests_local.py
+++ tests/test_pyscicat/tests_local.py
@@ -1,5 +1,5 @@
 from pyscicat.client import ScicatClient
-from pyscicat.model import RawDataset, Dataset, DatasetType
+from pyscicat.model import RawDataset, Ownable, Dataset, DatasetType
 from datetime import datetime
 import os
 import requests
@@ -32,31 +32,32 @@ def test_upload_dataset_requests():
 
     """ Testing if we can see a dataset in Scicat if it has been uploaded via requests but has been created via RawDataset model
       """
+
+    ownable = Ownable( ownerGroup="Magratheans", accessGroups=[])
     payload = RawDataset(
-        datasetName="a guide book",
+        datasetName="a new guide book",
         path="/foo/bar",
         size=42,
         packedSize=0,
         owner=os.environ["SCICAT_USER"],
-        ownerGroup="Magratheans",
         contactEmail="slartibartfast@magrathea.org",
         creationLocation="Magrathea",
-        creationTime=datetime.isoformat(datetime.now()),
+        creationTime=datetime.strftime(datetime.now(), format="%y-%m-%dT%H:%M:%S.%sZ"),
         instrumentId="earth",
         proposalId="deepthought",
         dataFormat="planet",
         principalInvestigator="A. Mouse",
         sourceFolder="/foo/bar",
-        scientificMetadata={"a": "field"},
+        scientificMetadata={"type": "string", "value":{"a": "field"}},
         sampleId="gargleblaster",
-        accessGroups=[],
         type="raw",
         ownerEmail="scicatingestor@your.site",
         sourceFolderHost="s3.heartofgold.org",
         endTime=datetime.isoformat(datetime.now()),
         techniques = [],
         numberOfFiles=0,
-        numberOfFilesArchived=0
+        numberOfFilesArchived=0,
+        **ownable.dict()
     )
 
 
@@ -71,14 +72,17 @@ def test_upload_dataset_requests():
     for k in drop_list:
       del prep_dict[k]
 
-    r = requests.request('post', os.environ["BASE_URL"] + '/datasets', json= prep_dict,
+    r = requests.request('post', os.environ["BASE_URL"] + '/datasets', json= payload.dict(exclude_none=True),
                          headers={'Authorization': f'Bearer {token}'})
-    print(r)
+   # r = requests.request('post', os.environ["BASE_URL"] + '/datasets', json= prep_dict,
+   #                      headers={'Authorization': f'Bearer {token}'})
+    # with open("prep_dict.json", 'w') as f:
+    #     json.dump(prep_dict, f)
 
     q = requests.request('get', os.environ["BASE_URL"] + '/Datasets',
                          headers={'Authorization': f'Bearer {sci_clie._token}'})
 
-    assert payload["datasetName"] in q.text
+    assert payload.datasetName in q.text
 
 
 def test_upload_dataset_sci_cli():
@@ -88,7 +92,8 @@ def test_upload_dataset_sci_cli():
     sci_clie = ScicatClient(base_url=os.environ["BASE_URL"],
                             token=None, username=os.environ["SCICAT_USER"],
                             password=os.environ["SCICAT_PASSWORD"])
-    payload ={
+
+    payload = {
         "owner": "ingestor",
         "ownerGroup": "Magratheans",
         "accessGroups": [],
@@ -147,6 +152,72 @@ def test_upload_dataset_sci_cli():
     assert payload["datasetName"] in r.text
     print(r.text)
 
+def test_minimal_upload():
+    """ Creating a minimal payload to see if we can replicate the error we are getting in raw dataset.
+    """
+    sci_clie = ScicatClient(base_url=os.environ["BASE_URL"],
+                            token=None, username=os.environ["SCICAT_USER"],
+                            password=os.environ["SCICAT_PASSWORD"])
+    payload = {
+        "owner": "ingestor",
+        "ownerGroup": "Magratheans",
+        "accessGroups": [],
+        "dataFormat": "directory",
+        "principalInvestigator": "slartibartfast",
+        # "endTime": "2020-04-08T08:10:07.553Z",
+        "creationLocation": "my_test_instrument",
+        # "scientificMetadata": {},
+        "owner": "ingestor",
+        #  "ownerEmail": "slartibartfast@magrathea.com",
+        # "orcidOfOwner": "https://orcid.org/0000-0000-0000-0000",
+        "contactEmail": "scicatingestor@org.org",
+        "sourceFolder": "new_test_4.txt",
+        # "sourceFolderHost": "s3.amazonaws.com",
+        "size": 0,
+        # "packedSize": 0,
+        "creationTime": "2023-03-08T08:10:06.713Z",
+        "type": "raw",
+        # "validationStatus": "string",
+        # "keywords": [],
+        # "description": "Description of my example dataset.",
+        "datasetName": "Minimal Dataset",
+        # "classification": "AV=medium,CO=low",
+        # "license": "string",
+        # "version": "rfi-file-monitor-0.2.0:extensions-0.1.3",
+        # "isPublished": True,
+        # "datasetlifecycle": {
+        #     "archivable": True,
+        #     "retrievable": True,
+        #     "publishable": True,
+        #     "dateOfDiskPurging": "2020-04-08T08:10:06.713Z",
+        #     "archiveRetentionTime": "2020-04-08T08:10:06.713Z",
+        #     "dateOfPublishing": "2020-04-08T08:10:06.713Z",
+        #     "isOnCentralDisk": True,
+        #     "archiveStatusMessage": "archive job sent",
+        #     "retrieveStatusMessage": "retrieve job sent",
+        #     "archiveReturnMessage": {},
+        #     "retrieveReturnMessage": {},
+        #     "exportedTo": "string",
+        #     "retrieveIntegrityCheck": True
+        # },
+        # # "history": [],
+        # "instrumentId": "string",
+        # "techniques": [
+        #     {
+        #         "pid": "string",
+        #         "name": "Example technique"
+        #     }
+        #     ]
+    }
+
+    with open("native_dict.json", 'w') as f:
+        json.dump(payload, f)
+
+    sci_clie._call_endpoint('post', 'Datasets', payload)
+
+    r = requests.request('get', os.environ["BASE_URL"] + '/Datasets',
+                         headers={'Authorization': f'Bearer {sci_clie._token}'})
+
 
 # def test_get_dataset(subtests):
 #     sci_clie = ScicatClient(base_url=os.environ["BASE_URL"],
@@ -183,3 +254,5 @@ def test_upload_dataset_sci_cli():
 #         accessGroups=["Vogons"]
 #     )
 #     sci_clie.update_dataset(payload, pid)
+
+
